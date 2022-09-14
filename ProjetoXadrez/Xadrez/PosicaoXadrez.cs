3babe0a341378603b580dbe03efd5d9d1e906c47
Diego Sacramento <diego-11-0@hotmail.com>
2022-09-13 23:20:22 -0300

    Criando a classe posicao xandrez

---
+++ ProjetoXadrez/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using tabuleiro;
+namespace Xadrez
+{
+    class PosicaoXadrez
+    {
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        public PosicaoXadrez()
+        {
+
+        }
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        public override string ToString()
+        {
+            return "" + Coluna + Linha;
+        }
+    }
+}
