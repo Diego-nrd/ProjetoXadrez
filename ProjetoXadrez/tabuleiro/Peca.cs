e13c121f369fa07892648863fd4e6d3c703215ed
Diego Sacramento <diego-11-0@hotmail.com>
2022-09-13 22:11:53 -0300

    Comecando a implementar o tabuleiro

---
+++ ProjetoXadrez/tabuleiro/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace tabuleiro
+{
+    class Peca 
+    {
+        public Posicao Posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qteMovimentos { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca()
+        {
+
+        }
+        public Peca(Posicao posicao, Cor cor, int qteMovimentos, Tabuleiro tab)
+        {
+            Posicao = posicao;
+            this.cor = cor;
+            this.qteMovimentos = qteMovimentos;
+            this.tab = tab;
+        }
+    }
+}
---
+++ ProjetoXadrez/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,22 @@
+﻿
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] pecas;
+
+        public Tabuleiro()
+        {
+
+        }
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linhas = linhas;
+            Colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
+    }
+}
